dbe42e1b9a8ce788b3fd4a29f9a9fb6fde03711e
Apfirebolt <aspper20@gmail.com>
2025-02-21 23:47:30 +0530

    user model and other boilerplate code added

---
+++ README.md
@@ -0,0 +1,9 @@
+# A Goal setter application in MERN
+
+![Express](https://img.shields.io/badge/Express.js-404D59?style=for-the-badge)
+![React](https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=white)
+![MongoDB](https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white)
+![Node](https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white)
+
+## Features
+
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+dotenv.config();
+import connectDB from "./config/db.js";
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API is running...' });
+  }
+);
+
+app.use("/api/users", userRoutes);
+
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
